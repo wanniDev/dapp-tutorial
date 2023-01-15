d70f696223301e0d57f31fab7abbe8a357d0fc98
Choi Hyung Joong (Wanni) <81374655+wanniDev@users.noreply.github.com>
2023-01-15 19:03:25 +0900

    solidity와 truffle을 이용하여, 스마트 컨트랙트 배포하기 (#1)
    * Feat: 스마크 컨트랙트 관련 코드들을 보관하기위해 conract 디렉토리 생성 및 트러플 적용
    
    * Feat: truffle 설정파일을 통해 스마트컨트랙트 배포 경로 및 트랜잭션 발생시 소모할 가스비 설정
    
    - 우선 테스트용으로 ganache 활용
    - 설정 파일 학습을 위해 기존 config 파일은 백업용으로 남김
    
    * Feat: 사과 구매, 판매, 조회관련 트랜잭션을 수행하기 위한 솔리디티 코드 구현
    
    * Fix: 사과 판매 함수중에 transfer 부분에 payable 키워드 추가
    
    From the docs page Solidity v0.8.0 Breaking Changes:
    The global variables tx.origin and msg.sender have the type address instead of address payable.
    One can convert them into address payable by using an explicit conversion, i.e., payable(tx.origin) or payable(msg.sender).
    
    * Feat: 스마트 컨트랙트 배포 스크립트 작성
    
    * Fix: 트러플 배포시 기본적으로 'development' 네트워크를 사용하는 부분에 맞게 설정 명 변경
    
    * Remove: 없는 스크립트 명령어 삭제

---
+++ contract/truffle-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      gas: 1000000
+    }
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.17", // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+      }
+    }
+  }
+};
