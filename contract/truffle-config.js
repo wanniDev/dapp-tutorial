c4d2216b5b388c7723bd948c8e57ee5a604efd71
wanniDev <wannidev0928@gmail.com>
2023-01-15 18:45:02 +0900

    Fix: 트러플 배포시 기본적으로 'development' 네트워크를 사용하는 부분에 맞게 설정 명 변경

--- contract/truffle-config.js
+++ contract/truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     contracts_directory: "./contracts",
     contracts_build_directory: "../src/main/resources/contract/abi",
-    ganache: {
+    development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
