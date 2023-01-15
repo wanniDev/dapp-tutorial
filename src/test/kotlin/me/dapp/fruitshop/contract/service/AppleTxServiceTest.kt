034512bb286aa6123f5dea346a7db2aef6194317
wanniDev <wannidev0928@gmail.com>
2023-01-15 21:22:09 +0900

    Test: 과일 구매 테스트코드 작성

---
+++ src/test/kotlin/me/dapp/fruitshop/contract/service/AppleTxServiceTest.kt
@@ -0,0 +1,30 @@
+package me.dapp.fruitshop.contract.service
+
+import me.dapp.fruitshop.AbstractTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class AppleTxServiceTest: AbstractTest() {
+    @Autowired
+    private lateinit var appleTxService: AppleTxService
+
+    @BeforeEach
+    fun init() {}
+
+    @AfterEach
+    fun cleanup() {}
+
+    @Test
+    @DisplayName("과일(사과) 구매")
+    fun buy() {
+        val abiName = "Shop"
+
+        var result = appleTxService.buyApple(abiName)
+
+        assertThat(result).isNotNull()
+    }
+}
